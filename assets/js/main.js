570a2cb90e69e88a6de44ef82ad7118585d3b2ff
Moises Santos da Luz <113149089+MoisesLhuz@users.noreply.github.com>
2023-07-19 00:40:17 -0300

    first-version
    challenge bedimcode Design de site NFT responsivo usando HTML, CSS e JavaScript.

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+/* MENU SHOW */
+/* valida se constante existe ~ validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+/* MENU HIDDEN */
+/* valida de constante existe ~ validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link,
+    // we remove the show-menu ~ Quando clicarmos em um item de navegação remove o menu
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // when the scroll is greater than 50 viewport height, add the scroll-header class
+    // quando o scroll for maior que 50 viewport height, adicione a classe scroll-header
+    this.scrollY >=50 ? header.classList.add('header-bg')
+                      : header.classList.remove('header-bg')
+    
+    window.addEventListener('scroll', scrollHeader);                    
+}
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+//window.sr = ScrollReveal({reset: true})
+
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 2500,    
+});
+
+sr.reveal(`.home__images`, {distance: '120px', delay: 400} );
+sr.reveal(`.home__title`, {delay: 1000} );
+sr.reveal(`.home__description`, {delay: 1200} );
+sr.reveal(`.home__button`, {delay: 1400} );
+sr.reveal(`.home__footer`, {delay: 1600} );
+sr.reveal(`.home__data div`, { origin: 'right', interval: 100, delay: 1800} );
